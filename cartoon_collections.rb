a15918f15dd8599911d7929620a5c92b29df940d
riley soloner <rileysoloner@gmail.com>
2022-08-11 14:03:05 -0400

    commit tree 9c5ebea696889916b3ef95c91ad6a874cd501274

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(0) do |dwarf, i|
+    puts "#{i + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.map do |element|
+    "#{element.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 4 }
+    # end
+  # x = calls.map do |call|
+  #   call.length > 4 ? true : false
+  # end
+  # x.include? true
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |snack|
+   if cheese_types.include?(snack)
+    return snack
+   end
+  # return nil
+  end
 end
